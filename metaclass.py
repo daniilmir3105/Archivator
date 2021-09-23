dba9750e42045a719f0efb768de82aff031bafa8
daniilmir3105 <miroshnichienko.daniil@gmail.com>
2021-09-23 14:43:56 +0300

    make setter and getter in metaclass.py

--- metaclass.py
+++ metaclass.py
@@ -8,7 +8,39 @@ class BaseClass(metaclass=ABCMeta):
     # this is base field(there will be path to files or maps
     __field = ''
 
-    # this will be
+    # this will be a setter 
+    @abstractmethod
+    @property
+    def basic_method(self):
+        '''
+        This abstract method will be base for other methods(*setters and getters).
+        :param variable: must be string(path to file or map)
+        :return: nothing 
+        '''
+        pass
+
+
+    # this will be setter for our field 
+    @abstractmethod
+    @basic_method.setter
+    def set_field(self, variable):
+        '''
+        This abstract method will be base for other methods.
+        :param variable: must be string(path to file or map)
+        :return: nothing 
+        '''
+        self.__field = variable
+
+
+    @abstractmethod
+    @basic_method.getter
+    def get_field(self): 
+        '''
+        This abstract method will get this field.
+        :param: none 
+        :return: field
+        ''' 
+        return self.__field
 
     @abstractmethod
     def method(self, variable):

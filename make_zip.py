1feed8d940cf0b5bde3511ff159e6a8968cf7afd
Daniil <miroshnichienko.daniil@gmail.com>
2021-10-14 14:58:42 +0300

    add main-method make_zip_archive() to make zip-archive

--- make_zip.py
+++ make_zip.py
@@ -9,3 +9,16 @@ class Zip(Base):
     This class will have fields and methods, that will make zip-archives.
     '''
 
+    @classmethod
+    def make_zip_archive(self, path_from, path_to, file_name):
+        '''
+        This method will have
+        :param path_from: string(path of files and maps, that must be archived)
+        path_to: string(path to files and maps)
+        file_name: string(name of zip-archive)
+        :return: none
+        '''
+
+        shutil.make_archive(base_name=path_to + '/' + file_name, format='zip', root_dir=path_from)
+        # return 0
+        pass

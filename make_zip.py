f7869de975c724a8336a292eea9105cc486c4d31
Daniil <miroshnichienko.daniil@gmail.com>
2021-10-14 14:16:19 +0300

    Start to work right with make_zip.py

--- make_zip.py
+++ make_zip.py
@@ -1,4 +1,5 @@
 import metaclass as mc
+import shutil
 # from abc import abstractmethod, abstractproperty
 
 Base = mc.BaseClass()
@@ -8,13 +9,3 @@ class Zip(Base):
     This class will have fields and methods, that will make zip-archives.
     '''
 
-    __map_path = '' 
-
-    # this is setter for our map-path
-    def set_map_path(self, var): #-> Base.set_field()
-        self.__map_path = var
-
-    # this is getter for our map-path
-    def get_map_path(self): #-> Base.set_field()
-        return self.__map_path
-        # pass

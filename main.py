d00b4d52c8e640e501aa94bbd45d1bdbedc422c6
Daniil <miroshnichienko.daniil@gmail.com>
2021-10-14 17:53:24 +0300

    add code to main.py, that will archivate something

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+import make_zip
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -11,6 +12,13 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('world!')
+    # this is basic object for working with files and maps
+    obj_for_archivating = make_zip.Zip()
+    obj_for_archivating.basic_method = r'C:\Users\Daniil\Documents\BioWare'
+    map_path = obj_for_archivating.basic_method
+
+    obj_for_archivating.make_zip_archive(path_from=map_path, path_to=r'', file_name='archive')
+    # print_hi('world!')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

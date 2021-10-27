037a3f6617fd7afa9bed79b6c4dbe4b8b13b7640
Daniil <miroshnichienko.daniil@gmail.com>
2021-10-27 20:28:53 +0300

    add downloader for youtube

--- copy_file.py
+++ copy_file.py
@@ -1,8 +1,46 @@
 from metaclass import BaseClass
-import shutil
+# import shutil
+# import abc
+import pafy
 
 class Copying(BaseClass):
     '''
-    This class will have methods for copying files and fields.
+    This class will have methods to download videos from yuotube.
     '''
 
+    # this is variable, that will have our url
+    __url_var = ''
+
+    @property
+    def url_function(self):
+        pass
+
+    @url_function.setter
+    def url_function(self, var):
+        '''
+        this is setter for __url_var
+        :param var: string, url
+        :return: nothing
+        '''
+
+        self.__url_var = var
+
+    @url_function.getter
+    def url_function(self):
+        '''
+
+        :return: string, __url_var
+        '''
+
+        return self.__url_var
+
+    @classmethod
+    def copy_file(self, url, path_to):
+        '''
+
+        :param url:
+        :return:
+        '''
+
+        pafy_obj = pafy.new(url=url)
+        pass
--- main.py
+++ main.py
@@ -25,3 +25,4 @@ if __name__ == '__main__':
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# https://youtu.be/ZSVoNDT4m3U
